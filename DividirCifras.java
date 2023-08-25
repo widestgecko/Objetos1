4ca32606761d725493a3f9225d159ea54e0ed18c
Antonio <rubengecko7@gmail.com>
2023-08-25 16:08:23 -0600

    Tarea

--- DividirCifras.java
+++ DividirCifras.java
@@ -3,11 +3,11 @@
 import java.util.Scanner;
 
 public class DividirCifras {
-    public static void main(String[] args) {
+    public static void main(String[] args, Scanner scanner) {
         System.out.print("\033[H\033[2J"); System.out.flush();
 
         System.out.print("Dame un numero de 3 cifras ?");
-        int num=new Scanner(System.in).nextInt();
+        int num=scanner.nextInt();
 
         int centenas = num/100;
         int decenas = (num - centenas*100)/10;
