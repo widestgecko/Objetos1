7751165cf187f44a0b9a05a617f28842df82aa93
Antonio <rubengecko7@gmail.com>
2023-08-30 13:32:22 -0600

    Actividad 4 - Programas elaborados en clase

--- README.md
+++ README.md
@@ -27,3 +27,32 @@ _p12_ConvertirTemperatura.
 _p13_VolumenCilindrico.
 _p14_CalculoTiempo.    
 _p15_ConvertirADolares.
+
+### Actividad 3
+
+_p16_TrabajandoEnteros
+_p17_TrabajandoFlotantes
+_p18_TrabajandoConstantes
+_p19_TrabajandoCaracteres
+_p20_ConversionTipos
+
+### Actividad 4 
+
+_p21_VerificaNumero
+_p22_VerificaSuma
+_p23_ConvertirTemperaturas
+_p24_PagaTrabajadorv2
+
+### Actividad 5
+
+_p25_EstacionesAño
+_p26_TipoAngulo
+_p27_AceptarEstudiante
+_p28_LeyDeNewton
+
+### Actividad 6
+
+_p29_Calculadora
+_p30_DiaSemana
+_p31_NumerosRomanos
+_p32_PagoEstacionamiento
---
+++ _p24PagaTrabajador.java
@@ -0,0 +1,31 @@
+//Antonio Rubén Martínez Villegas
+//Sat Emulator
+//Actividad 4
+//30/08/2023
+
+import java.util.Scanner;
+
+public class _p24PagaTrabajador {
+    public static void main(String[] args) {
+        //Declaración de variables
+        int horas, extra;
+        float paga, total;
+        Scanner obj = new Scanner(System.in);
+        
+        //Sentenciamos al pobre desgraciado
+        System.out.println("Calculando la condena de un trabajador \n");
+        System.out.print("¿Cuantas horas estará en la fosa? "); horas = obj.nextInt();
+        System.out.print("¿Cuanto le pagan ? "); paga = obj.nextFloat();
+
+        if( horas > 40 ) {
+            extra = horas - 40;
+            total = (40*paga) + (extra * paga * 2);
+            System.out.printf("\nHoras extra: %d", extra);
+        }
+        else
+                total = horas * paga;
+        
+        //Le decimos al pobre desgraciado cuantas tazas de arróz podrá comprár
+        System.out.printf("\nPago total: %.2f", total);
+    }
+}
